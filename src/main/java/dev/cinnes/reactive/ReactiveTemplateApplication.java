439d0fb5d5aacbdccc78ff1a8b0c3616e8c3a328
Callum Innes <callum@innes.run>
2023-04-18 19:23:26 +0000

    Code formatting

--- src/main/java/dev/cinnes/reactive/ReactiveTemplateApplication.java
+++ src/main/java/dev/cinnes/reactive/ReactiveTemplateApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReactiveTemplateApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ReactiveTemplateApplication.class, args);
 	}
-
 }
