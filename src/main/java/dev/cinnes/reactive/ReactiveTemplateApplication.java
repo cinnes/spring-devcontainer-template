b4f036faa145cdd96e4928afb644ca66bad64ca0
Callum Innes <callum@innes.run>
2023-04-18 19:15:09 +0000

    Add example spec driven reactive api

---
+++ src/main/java/dev/cinnes/reactive/ReactiveTemplateApplication.java
@@ -0,0 +1,13 @@
+package dev.cinnes.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveTemplateApplication.class, args);
+	}
+
+}
---
+++ src/main/java/dev/cinnes/reactive/controller/TestController.java
@@ -0,0 +1,17 @@
+package dev.cinnes.reactive.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+import dev.cinnes.api.TestApi;
+
+@RestController
+public class TestController implements TestApi {
+
+    @Override
+    public Mono<String> hello(ServerWebExchange exchange) {
+        return Mono.just("Hello world!");
+    }
+}
